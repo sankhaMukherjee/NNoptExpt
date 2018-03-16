31ad34d7b1983c6fa9a5205c39ae38c22dfc8c3e
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2018-03-16 15:41:54 +0800

    NNmodel3 can finally get and set weights properly

--- src/lib/NNlib/NNmodel.py
+++ src/lib/NNlib/NNmodel.py
@@ -634,7 +634,7 @@ class NNmodel2():
     [description]
     '''
 
-    @lD.log(logBase + '.NNmodel.__init__')
+    @lD.log(logBase + '.NNmodel2.__init__')
     def __init__(logger, self, inpSize, opSize, layers, activations):
         '''generate a model that will be used for optimization.
         
@@ -719,7 +719,7 @@ class NNmodel2():
 
         return
 
-    @lD.log(logBase + '.NNmodel.fitAdam')
+    @lD.log(logBase + '.NNmodel2.fitAdam')
     def fitAdam(logger, self, X, y, N = 1000, **params):
         '''[summary]
         
@@ -779,7 +779,7 @@ class NNmodel2():
 
         return weights
 
-    @lD.log( logBase + '.NNmodel.getWeights' )
+    @lD.log( logBase + '.NNmodel2.getWeights' )
     def getWeights(logger, self):
         '''[summary]
         
@@ -814,7 +814,7 @@ class NNmodel2():
 
         return weights
 
-    @lD.log( logBase + '.NNmodel.setWeights' )
+    @lD.log( logBase + '.NNmodel2.setWeights' )
     def setWeights(logger, self, weights, sess):
         '''set weights for a sesstion
         
@@ -847,7 +847,7 @@ class NNmodel2():
 
         return
 
-    @lD.log( logBase + '.NNmodel.errorValW' )
+    @lD.log( logBase + '.NNmodel2.errorValW' )
     def errorValW(logger, self, X, y, weights):
 
         errVal = None
@@ -867,7 +867,7 @@ class NNmodel2():
 
         return errVal
 
-    @lD.log( logBase + '.NNmodel.errorValWs' )
+    @lD.log( logBase + '.NNmodel2.errorValWs' )
     def errorValWs(logger, self, X, y, weightsList):
 
         errVals = []
@@ -890,4 +890,164 @@ class NNmodel2():
             logger.error( 'Unable to make a prediction: {}'.format(str(e)) )
 
         return errVals
+
+class NNmodel3():
+    '''[summary]
+    
+    [description]
+    '''
+
+    @lD.log(logBase + '.NNmodel3.__init__')
+    def __init__(logger, self, inpSize, opSize, layers, activations):
+        '''generate a model that will be used for optimization.
+        
+        Similar to the previous function, except that it totally does away
+        with saving the state at any time. This will significantly save 
+        on the time required to save the state and will speed things up. 
+        
+        Decorators:
+            lD.log
+        
+        Arguments:
+            logger {logging.Logger} -- Inserted by the decorator
+            self {instance of class} -- inserted by the class
+            inpSize {[type]} -- [description]
+            layers {[type]} -- [description]
+            activations {[type]} -- [description]
+        '''
+
+        self.modelOK = False
+        self.checkPoint = None
+        self.optimizer = None
+
+        self.fitted = False
+        self.currentErrors = None
+
+        try:
+
+            self.NNmodelConfig = json.load(open('../config/NNmodelConfig.json'))
+
+            logger.info('Generating a new model')
+            self.inpSize = inpSize
+            
+            self.Inp     = tf.placeholder(dtype=tf.float32, shape=inpSize, name='Inp')
+            self.Op      = tf.placeholder(dtype=tf.float32, shape=opSize, name='Op')
+            
+            self.allW         = []
+            self.allWPH       = []
+            self.allAssignW   = []
+
+            self.allB         = []
+            self.allBPH       = []
+            self.allAssignB   = []
+
+
+            self.result  = None
+
+            prevSize = inpSize[0]
+            for i, l in enumerate(layers):
+
+                tempW       = tf.Variable( 0.1*(np.random.rand(l, prevSize) - 0.5), dtype=tf.float32, name='W_{}'.format(i) )
+                tempWPH     = tf.placeholder(dtype=tf.float32, shape=(l, prevSize), name='PHW_{}'.format(i))
+                tempAssignW = tf.assign(tempW, tempWPH, name='AssignW_{}'.format(i))
+
+                tempB       = tf.Variable( 0, dtype=tf.float32, name='B_{}'.format(i) )
+                tempBPH     = tf.placeholder(dtype=tf.float32, shape=tuple(), name='PHB_{}'.format(i))
+                tempAssignB = tf.assign(tempB, tempBPH, name='AssignB_{}'.format(i))
+
+                self.allW.append( tempW )
+                self.allWPH.append( tempWPH )
+                self.allAssignW.append( tempAssignW )
+
+                self.allB.append( tempB )
+                self.allBPH.append( tempBPH )
+                self.allAssignB.append( tempAssignB )
+
+
+                if i == 0:
+                    self.result = tf.matmul( tempW, self.Inp ) + tempB
+                else:
+                    self.result = tf.matmul( tempW, self.result ) + tempB
+
+                prevSize = l
+
+                if activations[i] is not None:
+                    self.result = activations[i]( self.result )
+
+            self.err = tf.sqrt(tf.reduce_mean((self.Op - self.result)**2))
+
+            self.init    = tf.global_variables_initializer()
+            self.modelOK = True
+
+        except Exception as e:
+            logger.error('Unable to geberate the required model: {}'.format(str(e)))
+
+
+        return
+
+    @lD.log(logBase + '.NNmodel3.getWeights')
+    def getWeights(logger, self, sess=None):
+        '''[summary]
+        
+        [description]
+        
+        Parameters
+        ----------
+        logger : {[type]}
+            [description]
+        self : {[type]}
+            [description]
+        '''
+
+        weights = None
+        try:
+
+            if sess is not None:
+                weights = sess.run( self.allW + self.allB  )
+                return weights
+
+            with tf.Session() as sess:
+                sess.run(self.init)
+                weights = sess.run( self.allW + self.allB )
+                return weights
+
+        except Exception as e:
+            logger.error('Unable to get the weights: {}'.format(str(e)))
+
+
+        return weights
+
+    @lD.log(logBase + '.NNmodel3.setWeights')
+    def setWeights(logger, self, weights, sess):
+        '''[summary]
+        
+        [description]
+        
+        Parameters
+        ----------
+        logger : {[type]}
+            [description]
+        self : {[type]}
+            [description]
+        weights : {[type]}
+            [description]
+        sess : {[type]}
+            [description]
+        '''
+
+        try:
+            Nw = len(self.allW)
+            Ws = weights[:Nw]
+            Bs = weights[Nw:]
+
+            for i, (w, mW) in enumerate(zip(Ws, self.allAssignW)):
+                sess.run(mW, feed_dict={ 'PHW_{}:0'.format(i) : w } )
+
+            for i, (b, mW) in enumerate(zip(Bs, self.allAssignB)):
+                sess.run(mW, feed_dict={ 'PHB_{}:0'.format(i) : b } )
+
+        except Exception as e:
+            logger.error('Unable to set the weights ...: {}'.format(str(e)))
+
+        return
 
--- src/modules/NNmodule/NNmodule.py
+++ src/modules/NNmodule/NNmodule.py
@@ -98,6 +98,45 @@ def checkNNmodel1(logger):
 
     return
 
+@lD.log(logBase + '.checkNNmodel3')
+def checkNNmodel3(logger):
+    '''Check whether the NNmodel is working find
+    
+    [description]
+
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger function
+    '''
+
+    # X = np.random.rand(2, 10000)
+    # y = (  2*np.sin(X[0, :]) + 3*np.cos(X[1, :]) ).reshape(1, -1)
+    # y = (  2*X[0, :] + 3*X[1, :] ).reshape(1, -1)
+
+    print('We are in the NNmodule')
+    inpSize     = (2, None)
+    opSize      = (1, None)
+    layers      = (5, 8, 1)
+    activations = [tf.tanh, tf.tanh, None]
+    model1      = NNmodel.NNmodel3(inpSize, opSize, layers, activations)
+
+    with tf.Session() as sess:
+        sess.run(model1.init)
+        weights = model1.getWeights()
+        print(weights)
+        weights = [w+5 for w in weights]
+        
+    with tf.Session() as sess:
+        sess.run(model1.init)
+
+        model1.setWeights(weights, sess)
+        weights1 = model1.getWeights(sess)
+        
+    print(weights1)
+    
+    return
+
 @lD.log(logBase + '.main')
 def main(logger):
     '''main function for module1
@@ -113,7 +152,8 @@ def main(logger):
     '''
 
     # checkNNmodel1()
-    checkNNmodel1()
+    # checkNNmodel1()
+    checkNNmodel3()
 
     return
 
